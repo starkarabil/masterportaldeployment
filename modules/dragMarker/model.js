68186d8778bf5c2dc416f07e02a54aae45fc6d6a
Michael Bieler <michael.bieler@gv.hamburg.de>
2017-02-15 07:53:55 +0100

    add Radio requestAddress in dragMarker

--- modules/dragMarker/model.js
+++ modules/dragMarker/model.js
@@ -59,7 +59,8 @@ define(function (require) {
                 "handleMoveEvent": this.handleMoveEvent,
                 "setPosition": this.setPosition,
                 "hide": this.hideMarker,
-                "show": this.showMarker
+                "show": this.showMarker,
+                "requestAddress": this.sendOrRequestNearestAddress
             }, this);
 
             channel.reply({
@@ -72,7 +73,7 @@ define(function (require) {
             Radio.on("ReverseGeocoder", "addressComputed", this.setNearestAddress, this);
 
             // internal Listeners
-            this.listenTo(this, {"change:nearestAddress": this.newAddress});
+            this.listenTo(this, {"change:nearestAddress": this.sendNewAddress});
 
             // Prepare Map
             Radio.trigger("Map", "addLayerToIndex", [this.get("dragMarkerLayer"), Radio.request("Map", "getLayers").getArray().length]);
@@ -150,7 +151,7 @@ define(function (require) {
             this.get("dragMarkerLayer").getSource().clear();
             this.get("dragMarkerLayer").getSource().addFeature(pointFeature);
             this.set("coordinate", coordinate);
-            this.triggerNewCoordinate(coordinate);
+            this.requestNewAddress();
         },
 
         // replies specific coordinate of feature
@@ -205,19 +206,34 @@ define(function (require) {
             }
         },
 
-        // zoom auf Koordinate
+        // zoom der Map auf Koordinate
         zoomTo: function (coord) {
             Radio.trigger("MapView", "setCenter", coord, 7);
         },
 
-        // trigger new center coordinate
-        triggerNewCoordinate: function () {
+        // Requests "nearestAddress", indem der ReverseGeocoder angefragt wird.
+        requestNewAddress: function () {
             Radio.trigger("ReverseGeocoder", "request", this.get("coordinate"));
         },
 
-        // trigger newAddress gefunden. Kann auch Fehlerobjekt sein.
-        newAddress: function () {
+        // Wird auf listenTo "change:nearestAddress" registriert. Liefert Adressobjekt bzw. Fehlerobjekt aus.
+        sendNewAddress: function () {
             Radio.trigger("DragMarker", "newAddress", this.get("nearestAddress"))
+        },
+
+        // Wird auf Radio "requestAddress" ausgeführt. Prüft, ob gültige Adresse ermittelt wurde und triggert diese sofort bzw. initiiert eine neue Abfrage
+        sendOrRequestNearestAddress: function () {
+            var newAddress = this.get("nearestAddress");
+
+            if (_.has(newAddress, "error") === true) {
+                this.requestNewAddress(); // letztmalig wurde ein Fehlerobjekt zurückgemeldet. Daher neue Anfrage.
+            }
+            else if (_.isNull(newAddress) === true) {
+                this.requestNewAddress(); // noch keine Addresse ermittelt. Daher neue Anfrage.
+            }
+            else if (_.has(newAddress, "distance") === true) {
+                this.sendNewAddress(); // eine gültige Addresse wurde bereits ermittelt. Direkt triggern.
+            }
         },
 
         // INTERACTION EVENTS
@@ -264,7 +280,7 @@ define(function (require) {
 
         // stop Event
         handleUpEvent: function () {
-            this.triggerNewCoordinate();
+            this.requestNewAddress();
             this.set("dragMarkerFeature", null);
             return false;
         }
