7da58bdd7b4a6d79962cc84fb46a6a9d99d98d8a
Beyel <BeyelSv@fhhnet.stadt.hamburg.de>
2020-05-04 15:42:19 +0200

    add first steps for new searchbar in vue with vuex store

--- src/App.vue
+++ src/App.vue
@@ -43,7 +43,6 @@ export default {
         class="masterportal-container"
     >
         <MainNav />
-        <Searchbar />
         <MapRegion class="map-region" />
         <Footer />
         <!-- <MapModuleDebug v-if="isDevMode" /> -->
