ddfcbe2b2a576aaeebdf3b158b7852a101c1f3e8
Burmester <BurmesKi@fhhnet.stadt.hamburg.de>
2020-06-09 14:00:40 +0200

    add unittest BG-677

--- test/unittests/modules/searchbar/specialWFS/model.test.js
+++ test/unittests/modules/searchbar/specialWFS/model.test.js
@@ -4,6 +4,8 @@ import {expect} from "chai";
 
 describe("modules/searchbar/specialWFS", function () {
     var model = {},
+        multiPolygonFeaturesWithInteriorPolygons,
+        multiPolygonFeaturesWithoutInteriorPolygons,
         config = {
             "minChars": 5,
             "maxFeatures": 1,
@@ -49,6 +51,10 @@ describe("modules/searchbar/specialWFS", function () {
         };
 
     before(function () {
+        const utilModel = new Util();
+
+        multiPolygonFeaturesWithInteriorPolygons = utilModel.getDescribeFeatureTypeResponse("resources/testFeaturesBplanMultiPolygonWithInteriorPolygon.xml");
+        multiPolygonFeaturesWithoutInteriorPolygons = utilModel.getDescribeFeatureTypeResponse("resources/testFeaturesBplanMultiPolygonWithoutInteriorPolygon.xml");
         model = new Model(config);
     });
 
@@ -99,20 +105,66 @@ describe("modules/searchbar/specialWFS", function () {
     });
 
     describe("getInteriorAndExteriorPolygonMembers", function () {
-        let multiPolygonFeatures,
-            utilModel;
+        it("should return an array with two arrays. First one with coordiantes, second one with indices", function () {
+            const elements = Array.from(multiPolygonFeaturesWithInteriorPolygons.getElementsByTagNameNS("*", "polygonMember")),
+                expectedResult = [
+                    [
+                        [
+                            "560313.401", "5934298.407",
+                            "560294.587", "5934301.811",
+                            "560285.063", "5934303.535",
+                            "560271.224", "5934294.941",
+                            "560268.175", "5934295.831",
+                            "560258.461", "5934229.981"
+                        ],
+                        [
+                            "561016.997", "5933835.861",
+                            "561017.451", "5933843.541",
+                            "561061.576", "5933841.794"
+                        ],
+                        [
+                            "561099.048", "5933643.820",
+                            "561099.267", "5933651.168",
+                            "561323.539", "5933598.007",
+                            "561323.352", "5933600.283"
+                        ]
+                    ],
+                    [1, 2]
+                ];
 
-        before(function () {
-            utilModel = new Util();
-            multiPolygonFeatures = utilModel.getDescribeFeatureTypeResponse2();
-            // multiPolygonFeatures = utilModel.createTestFeatures2("resources/testFeaturesBplanMultiPolygonWithInteriorPolygon.xml");
-            // multiPolygonMembers = {0: utilModel.getPolygonMembers(multiPolygonFeatures)};
+            expect(model.getInteriorAndExteriorPolygonMembers(elements)).to.deep.equal(expectedResult);
+        });
+        it("should return an array with two arrays. First one with, second one without content", function () {
+            const elements = Array.from(multiPolygonFeaturesWithoutInteriorPolygons.getElementsByTagNameNS("*", "polygonMember")),
+                expectedResult = [
+                    [
+                        [
+                            "560313.401", "5934298.407",
+                            "560294.587", "5934301.811",
+                            "560285.063", "5934303.535",
+                            "560271.224", "5934294.941",
+                            "560268.175", "5934295.831",
+                            "560258.461", "5934229.981"
+                        ]
+                    ],
+                    []
+                ];
+
+            expect(model.getInteriorAndExteriorPolygonMembers(elements)).to.deep.equal(expectedResult);
+        });
+        it("should return two empty arrays within an array", function () {
+            expect(model.getInteriorAndExteriorPolygonMembers([])).to.deep.equal([[], []]);
+        });
+        it("should return two empty arrays within an array", function () {
+            expect(model.getInteriorAndExteriorPolygonMembers("")).to.deep.equal([[], []]);
+        });
+        it("should return two empty arrays within an array", function () {
+            expect(model.getInteriorAndExteriorPolygonMembers(false)).to.deep.equal([[], []]);
+        });
+        it("should return two empty arrays within an array", function () {
+            expect(model.getInteriorAndExteriorPolygonMembers(NaN)).to.deep.equal([[], []]);
         });
 
-        it("should return an array with two arrays", function () {
-            console.log(multiPolygonFeatures);
-            console.log(model.getInteriorAndExteriorPolygonMembers(multiPolygonFeatures));
-            // expect(model.getInteriorAndExteriorPolygonMembers(polygonMembers).to.be.equal([["1 2 3 4 5"]]));
-        });
+
     });
 });
