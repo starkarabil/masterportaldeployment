2b80c4c8b430c5130b99268bc3df63e9e48ddee5
Dudek <DudekIn@fhhnet.stadt.hamburg.de>
2021-01-20 15:51:26 +0100

    BG-1525-stable: lint

--- modules/tools/print_/mapfish3PlotService.js
+++ modules/tools/print_/mapfish3PlotService.js
@@ -588,7 +588,7 @@ const PrintModel = Tool.extend(/** @lends PrintModel.prototype */{
      */
     getVisibleLayer: function () {
         let visibleLayerList = Radio.request("Map", "getLayers").getArray().filter(layer => {
-            return layer.getVisible() === true && "markerPoint" !== layer.get("name");
+            return layer.getVisible() === true && layer.get("name") !== "markerPoint";
         });
 
         visibleLayerList = this.sortVisibleLayerListByZindex(visibleLayerList);
